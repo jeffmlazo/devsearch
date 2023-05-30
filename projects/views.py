75d75ccf231c7e944d0679a26477f13558b7f074
Jeff Ray <jeffmlazo@yahoo.com>
2023-05-31 01:24:27 +0800

    Add main, navbar & initial content Add migrated Project table Add projects & single project page

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': "1",
+        'title': "Ecommerce Website",
+        'description': "Fully functional ecommerce website"
+    },
+    {
+        'id': "2",
+        'title': "Porfolio Website",
+        'description': "This was a project where I built out my portfolio"
+    },
+    {
+        'id': "3",
+        'title': "Social Network",
+        'description': "Awesome open source project I am still woking on"
+    },
+]
+
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    page = 'Hello, you are on the projects page'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    context = {'project': projectObj}
+    return render(request, "projects/single-project.html", context)
