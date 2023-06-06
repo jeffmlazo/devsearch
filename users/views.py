7bb5e7c032995c3fbb6b7cc827af881e954146fa
Jeff Ray <jeffmlazo@yahoo.com>
2023-06-07 01:32:54 +0800

    Add login aunthentication Add user logout functionality Add login page Add login & logout flash messages Add hiding of add project, logout, login/signup links

--- users/views.py
+++ users/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        # Checks in the database if the username and password match
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user is exist
+        if user is not None:
+            """
+            Creates a user session in the database in the user 
+            table and a session data will be added in the browser
+            """
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    """
+    This method will delete the session data from the database 
+    and in the browser
+    """
+    logout(request)
+    messages.error(request, 'User was logged out')
+    return redirect('login')
 
 
 def profiles(request):
