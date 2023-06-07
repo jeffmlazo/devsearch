e85028bb879aa65f5885d20f907b3cba6e6e05b2
Jeff Ray <jeffmlazo@yahoo.com>
2023-06-08 01:54:57 +0800

    Add dynamic popup messages Add uikit & main javascripts Update navigation links for Account Add custom forms of user registration Add css classes for registration form Update the look of login & registration using the theme

--- users/views.py
+++ users/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -15,24 +17,27 @@ def loginUser(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'Username does not exist')
+        if User.objects.filter(username=username).exists():
+            # try:
+            #     user = User.objects.get(username=username)
+            # except:
+            #     messages.error(request, 'Username does not exist')
 
-        # Checks in the database if the username and password match
-        user = authenticate(request, username=username, password=password)
+            # Checks in the database if the username and password match
+            user = authenticate(request, username=username, password=password)
 
-        # Check if the user is exist
-        if user is not None:
-            """
-            Creates a user session in the database in the user 
-            table and a session data will be added in the browser
-            """
-            login(request, user)
-            return redirect('profiles')
+            # Check if the user is exist
+            if user is not None:
+                """
+                Creates a user session in the database in the user 
+                table and a session data will be added in the browser
+                """
+                login(request, user)
+                return redirect('profiles')
+            else:
+                messages.error(request, 'Username OR password is incorrect')
         else:
-            messages.error(request, 'Username OR password is incorrect')
+            messages.error(request, 'Username does not exist')
 
     return render(request, 'users/login_register.html')
 
@@ -43,8 +48,28 @@ def logoutUser(request):
     and in the browser
     """
     logout(request)
-    messages.error(request, 'User was logged out')
+    messages.info(request, 'User was logged out!')
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            # The post data is temporarily saving it so that you can modify the data before saving it
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'Account was successfully created!')
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'An error has occured during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
@@ -62,3 +87,12 @@ def userProfile(request, pk):
     context = {'profile': profile, 'topSkills': topSkills,
                'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+    return render(request, 'users/account.html', context)
