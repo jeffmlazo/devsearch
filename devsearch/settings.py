f9f8f1845f56e81aa75d67db555d743431ec1abb
Jeff Ray <jeffmlazo@yahoo.com>
2023-06-21 01:40:24 +0800

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'livereload.middleware.LiveReloadScript',
+    # 'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = "devsearch.urls"
